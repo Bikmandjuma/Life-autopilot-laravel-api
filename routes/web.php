1daafae15ee44c3f2774183a0f4982af6e2d53c1
unknown <bikmangeek@gmail.com>
2026-05-18 23:56:38 +0200

    web modified

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return 'ShortSliceAI Laravel API running';
+    return 'Life autopilot API is running !';
 });
 
